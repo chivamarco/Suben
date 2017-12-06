bfa515d4b7649b98f3700a7f2e1066968dd78032
Marco Garcia <garciagarcia.marco@gmail.com>
2017-12-06 15:44:16 -0600

    Added Subruta interface, working on polyline maker

--- app/src/main/java/com/example/extermination/suben/App/AppConfig.java
+++ app/src/main/java/com/example/extermination/suben/App/AppConfig.java
@@ -9,7 +9,7 @@ public class AppConfig {
    public static String URL_LOGIN = "http://192.168.1.80/android_login_api/Source Files/login.php";
     //public static String URL_LOGIN = "http://10.0.0.6/android_login_api/Source%20Files/login.php";
     //public static String LOC_SENDER = "http://10.0.0.6/android_login_api/Source%20Files/saveloc.php";
-    //public static String LOC_SENDER = "http://10.0.0.6/android_login_api/Source%20Files/include/a.php";
+    public static String LOC_SENDER = "http://10.0.0.6/android_login_api/Source%20Files/include/a.php";
 
     // Server user register url
    //public static String URL_REGISTER = "http://10.0.0.6/android_login_api/Source%20Files/register.php";
